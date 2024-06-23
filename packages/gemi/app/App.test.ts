0bdb82e9d7475ed949618c06d45c77c677787b3f
Enes Tufekci <enesxtufekci@gmail.com>
2024-06-23 13:42:37 +0200

    app tests

--- packages/gemi/app/App.test.ts
+++ packages/gemi/app/App.test.ts
@@ -6,15 +6,36 @@ import { ViewRouter } from "../http/ViewRouter";
 
 import { createRoot } from "../client/createRoot";
 import { createElement } from "react";
+import { Controller } from "../http/Controller";
+import { HttpRequest } from "../http/HttpRequest";
+
+class TestController extends Controller {
+  test() {
+    return { data: { message: "test" } };
+  }
+  async foo(req: HttpRequest) {
+    const body = await req.input();
+
+    return { data: { success: true, body: body.toJSON() } };
+  }
+}
 
 class RootApiRouter extends ApiRouter {
   routes = {
     "/test": this.get(() => {
       return { data: { message: "hi" } };
     }),
+    "/foo": this.post(TestController, "foo"),
   };
 }
-class RootViewRouter extends ViewRouter {}
+class RootViewRouter extends ViewRouter {
+  routes = {
+    "/": this.view("Home", () => {
+      return { data: { message: "Home" } };
+    }),
+    "/about": this.view("About", [TestController, "test"]),
+  };
+}
 
 const Root = () => createElement("div");
 
@@ -25,11 +46,39 @@ const app = new App({
 });
 
 describe("App", () => {
-  test("should be defined", async () => {
+  test("api callback handler", async () => {
     const request = new Request("http://gemi.dev/api/test", {
       method: "GET",
     });
     const res = await app.handleRequest(request);
-    expect(res).toEqual({});
+    expect(res.data).toEqual({ message: "hi" });
+  });
+
+  test("view data callback handler", async () => {
+    const request = new Request("http://gemi.dev?json=true", {
+      method: "GET",
+    });
+    const res = await app.handleRequest(request);
+    expect(res.data).toEqual({ "/": { Home: { message: "Home" } } });
+  });
+
+  test("view controller handler", async () => {
+    const request = new Request("http://gemi.dev/about?json=true", {
+      method: "GET",
+    });
+    const res = await app.handleRequest(request);
+    expect(res.data).toEqual({ "/about": { About: { message: "test" } } });
+  });
+
+  test("api controller handler", async () => {
+    const request = new Request("http://gemi.dev/api/foo", {
+      method: "POST",
+      body: JSON.stringify({ data: 1 }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const res = await app.handleRequest(request);
+    expect(res.data).toEqual({ success: true, body: { data: 1 } });
   });
 });
