ec28a278600694f6b9510399a1f3779864f49082
Enes Tufekci <enesxtufekci@gmail.com>
2024-07-11 13:19:34 +0200

    update bin

---
+++ packages/gemi/scripts/prepare-bin.ts
@@ -0,0 +1,17 @@
+import { join } from "path";
+import { chmod } from "fs/promises";
+
+async function main() {
+  const binFilePath = join(process.cwd(), "dist/bin/index.js");
+  const file = Bun.file(binFilePath);
+  const content = await file.text();
+
+  const shebang = "#!/usr/bin/env bun\n";
+
+  await Bun.write("dist/bin/index.js", shebang + content);
+
+  // add chmod +x to the file
+  await chmod(binFilePath, 0o755);
+}
+
+main();
