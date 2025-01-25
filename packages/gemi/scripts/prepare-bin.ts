6d45b34988153676118573682b6c3f10dfa52929
Enes Tufekci <enesxtufekci@gmail.com>
2025-01-26 00:55:42 +0100

    fix build

--- packages/gemi/scripts/prepare-bin.ts
+++ packages/gemi/scripts/prepare-bin.ts
@@ -2,13 +2,13 @@ import { join } from "path";
 import { chmod } from "fs/promises";
 
 async function main() {
-  const binFilePath = join(process.cwd(), "dist/bin/index.js");
+  const binFilePath = join(process.cwd(), "dist/bin/gemi.js");
   const file = Bun.file(binFilePath);
   const content = await file.text();
 
   const shebang = "#!/usr/bin/env bun\n";
 
-  await Bun.write("dist/bin/index.js", shebang + content);
+  await Bun.write("dist/bin/gemi.js", shebang + content);
 
   // add chmod +x to the file
   await chmod(binFilePath, 0o755);
