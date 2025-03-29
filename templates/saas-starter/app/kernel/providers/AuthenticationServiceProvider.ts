f2000b78ac1f3872ccf317e9cfc44c82fee53958
Enes Tufekci <enesxtufekci@gmail.com>
2025-03-29 22:14:58 +0100

    Add defaults to auth service provider

--- templates/saas-starter/app/kernel/providers/AuthenticationServiceProvider.ts
+++ templates/saas-starter/app/kernel/providers/AuthenticationServiceProvider.ts
@@ -7,17 +7,27 @@ import { prisma } from "@/app/database/prisma";
 
 export default class extends AuthenticationServiceProvider {
   adapter = new PrismaAuthenticationAdapter(prisma);
+
+  // Adapt these options to your needs
+  sessionExpiresInHours = 24 * 30; // 30 days
+  // Sessions will be renewed if the user logs in within this time frame
+  sessionAbsoluteExpiresInHours = 24 * 30 * 6; // 6 months
+
+  // Change this true to only allow verified emails to login
   verifyEmail = false;
 
   async onSignUp(user: any, token: string) {
-    // Send email verification
+    // This hook will be called when a user signs up
+    // You can send email verification here
   }
 
-  onForgotPassword(user: any, token: string) {
-    // send forgot password email
+  async onForgotPassword(user: any, token: string) {
+    // This hook will be called when a user requests a password reset
+    // You can send password reset email here
   }
 
-  onMagicLinkCreated(user: any, args: any) {
-    // send magic link email
+  async onMagicLinkCreated(user: any, args: any) {
+    // This hook will be called when a magic link is created
+    // You can send magic link email here
   }
 }
