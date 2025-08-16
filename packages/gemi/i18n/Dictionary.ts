33e6726dead9e9e4c999f940b1871992e47a661e
Enes Tufekci <enesxtufekci@gmail.com>
2025-08-16 11:33:42 +0200

    i18n: make locale optional

--- packages/gemi/i18n/Dictionary.ts
+++ packages/gemi/i18n/Dictionary.ts
@@ -1,5 +1,7 @@
+import { I18n } from "../facades";
 import { parseTranslation } from "../utils/parseTranslation";
 import type { ParseTranslationParams, Prettify } from "../utils/type";
+import { I18nServiceContainer } from "./I18nServiceContainer";
 
 type Translations = Record<string, Record<string, string>>;
 
@@ -23,20 +25,36 @@ export class Dictionary<T extends Translations> {
     return this.dictionary[key];
   }
 
-  render<K extends keyof T, U extends keyof T[K]>(
+  render<
+    K extends keyof T,
+    U extends keyof T[K],
+    R = Prettify<ParseTranslationParams<T[K][U]>>,
+  >(
     key: K,
-    locale: U,
-    ...args: ParamsOrNever<Prettify<ParseTranslationParams<T[K][U]>>>
+    ...args: R extends Record<string, never>
+      ? [args?: { locale?: U | (string & {}) }]
+      : [args: { locale?: U | (string & {}); params: R }]
   ) {
+    const {
+      locale = I18n.locale() ??
+        I18nServiceContainer.use().service.defaultLocale ??
+        "en-US",
+      params,
+    } = {
+      params: {},
+      ...args[0],
+    };
+
     if (typeof window !== "undefined") {
       throw new Error("Cannot use render in the browser");
     }
+
     if (!this.dictionary?.[key]?.[locale]) {
       throw new Error(
         `Translation not found for ${String(key)} in ${String(locale)}`,
       );
     }
-    return parseTranslation(this.dictionary[key][locale], args[0] ?? {});
+    return parseTranslation(this.dictionary[key][locale], params ?? {});
   }
 
   static create<const T extends Translations>(name: string, translations: T) {
