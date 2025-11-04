4faedb6e5c755929498029246355a2c97d174706
Enes Tufekci <enesxtufekci@gmail.com>
2025-11-04 23:53:47 +0100

    fix trailing slash

--- packages/gemi/utils/applyParams.ts
+++ packages/gemi/utils/applyParams.ts
@@ -2,24 +2,29 @@ export function applyParams<T extends string>(
   url: T,
   params: Record<string, string | number | undefined>,
 ): string {
-  return url
-    .replace(/:([^/]+[*?]?)/g, (_, key) => {
-      const hasSuffix = key.endsWith("?") || key.endsWith("*");
-      const paramName = hasSuffix ? key.slice(0, -1) : key;
-      const value = params[paramName];
+  return (
+    url
+      .replace(/:([^/]+[*?]?)/g, (_, key) => {
+        const hasSuffix = key.endsWith("?") || key.endsWith("*");
+        const paramName = hasSuffix ? key.slice(0, -1) : key;
+        const value = params[paramName];
 
-      if (value === undefined) {
-        if (hasSuffix) {
-          return ""; // Remove the optional segment if no value is provided
+        if (value === undefined) {
+          if (hasSuffix) {
+            return ""; // Remove the optional segment if no value is provided
+          }
+          // @ts-ignore
+          if (import.meta.env.DEV) {
+            throw new Error(`Missing parameter: ${paramName}`);
+          }
+          console.error(`Missing parameter: ${paramName} in URL: ${url}`);
         }
-        // @ts-ignore
-        if (import.meta.env.DEV) {
-          throw new Error(`Missing parameter: ${paramName}`);
-        }
-        console.error(`Missing parameter: ${paramName} in URL: ${url}`);
-      }
 
-      return String(value);
-    })
-    .replace(/\/\//g, "/"); // Remove any double slashes caused by optional parameters
+        return String(value);
+      })
+      // remove double slashes
+      .replace(/\/\//g, "/")
+      // remove trailing slash
+      .replace(/\/$/, "")
+  );
 }
